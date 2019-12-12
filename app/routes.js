05dfc0cfbbfd81085272cab3ca871dd0d94fecac
Andy811 <andygood811@gmail.com>
2019-12-12 09:09:09 +0800

    first

--- app/routes.js
+++ app/routes.js
@@ -75,46 +75,58 @@ module.exports = function(app) {
  })
 //reservation.ejs insert and search
 
+
  app.get('/reservation', isLoggedIn, function(req, res){
   res.render('reservation.ejs', {
-   user:req.user
+   user:req.user,
+   data:'幹'
   });
  });
-app.post('/search',urlencodedParser, function(req, res) {
-console.log(req.body);
+ 
+ 
 
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "123456",
-    database: "nodejs_login",
-});
-var data = {};
-var searchroom = req.body.searchroomid
-var searchstart = req.body.searchstarttime
-var searchend = req.body.searchendtime
-var searchdate = req.body.searchopendate
-var searchdepartment = req.body.searchdepartment
-var searchtopic = req.body.searchtopic
-var sqlforsearch = 'select * from reservation where (roomid="'+ searchroom +'" OR starttime="'+ searchstart +'"OR endtime="'+ searchend +'"OR opendate="'+ searchdate +'"OR department="'+ searchdepartment +'"OR meetingname="'+ searchtopic +'")'
- con.query(sqlforsearch, function(err, rows) {
-     console.log('搜尋結果',rows);
-     data.reservation = rows;
-     if (err)  {
-            res.redirect('errorre');
-            } else {
-              res.render('searchpage',{data:data.reservation});
-            }
-     
-    })
-
-app.get('/searchpage', isLoggedIn, function(req, res){
-  res.render('searchpage', {
-   data:data.reservation,
+  app.post('/search',urlencodedParser, function(req, res) {
+  console.log(req.body);
+  
+    var con = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "123456",
+      database: "nodejs_login",
   });
- });
-});
+
+  var data = {};
+  var searchroom = req.body.searchroomid
+  var searchstart = req.body.searchstarttime
+  var searchend = req.body.searchendtime
+  var searchdate = req.body.searchopendate
+  var searchdepartment = req.body.searchdepartment
+  var searchtopic = req.body.searchtopic
+
+  var sqlforsearch = 'select * from reservation where (roomid="'+ searchroom +'" OR starttime="'+ searchstart +'"OR endtime="'+ searchend +'"OR opendate="'+ searchdate +'"OR department="'+ searchdepartment +'"OR meetingname="'+ searchtopic +'")'
+  //var sqlforsearch = 'select meetingName from reservation where opendate='+searchdate
+  //測試
+
+  con.query(sqlforsearch, function(err, rows) {
+      console.log('搜尋結果',rows);
+      data.reservation = rows;     
+      
+      data.reservation = rows;
+      if (err)  {
+              res.redirect('errorre');
+              } else {
+                res.render('searchpage',{data:data.reservation});
+              }
+      })
+  app.get('/searchpage', isLoggedIn, function(req, res){
+    res.render('searchpage', {
+    data:data.reservation,
+    });
+  });
+  });
     
+
+
  app.post('/datainsert' ,urlencodedParser, function(req, res) {
       console.log(req.body);
       var con = mysql.createConnection({
@@ -125,15 +137,15 @@ app.get('/searchpage', isLoggedIn, function(req, res){
 		});
     
 		con.connect(function(err) {
-		 if (err) throw err;
+       var start = req.body.StartTime
+       var end = req.body.endtime
+       var room = req.body.meetingroom
+       var MeetingDate = req.body.opendate
+       var department = req.body.department
+       var meetingname = req.body.meetingname
+      if (err) throw err;
       console.log("Connected!");
-      var start = req.body.StartTime
-      var end = req.body.endtime
-      var room = req.body.meetingroom
-      var MeetingDate = req.body.opendate
-      var department = req.body.department
-      var meetingname = req.body.meetingname
-		  var sql = "INSERT INTO reservation (roomid,starttime,endtime,opendate,department,meetingname ) VALUES ('"+room+"','"+start+ "' ,'"+end+ "','"+MeetingDate+ "','"+department+ "','"+meetingname+ "')";
+		  var sql = "INSERT INTO reservation (roomid,starttime,endtime,opendate,department,meetingname,meetingid ) VALUES ('"+room+"','"+start+ "' ,'"+end+ "','"+MeetingDate+ "','"+department+ "','"+meetingname+ "',123456)";//meetingid暫用
       console.log(sql);
 		  con.query(sql, function (err, result) {
           console.log(result);
@@ -144,21 +156,12 @@ app.get('/searchpage', isLoggedIn, function(req, res){
                     res.redirect('complete')
                   }
                 });
-        
-        
-        
-        
-        
-        
         });
-
 			});
 		};
 
 	
 
-
-	
 
 		// console.log(con);
 function isLoggedIn(req, res, next){
