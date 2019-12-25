bbe74de455514a372736e7c99cb2870a9e593f21
Andy811 <andygood811@gmail.com>
2019-12-25 16:32:29 +0800

    巢狀迴圈讀取物件失敗

--- app/routes.js
+++ app/routes.js
@@ -5,8 +5,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false });
 var passport = require('passport');
 var router = express.Router();
 
-
-
+var one_week = new Array(7)//一個禮拜的日期
 module.exports = function (app) {
   app.get(/(.*)\.(jpg|gif|png|ico|css|js|txt)/i, function (req, res) {
     res.sendfile(__dirname + "/" + req.params[0] + "." + req.params[1], function (err) {
@@ -74,7 +73,7 @@ module.exports = function (app) {
     res.redirect('/');
   })
   //reservation.ejs insert and search
-  var con = mysql.createConnection({
+  var con = mysql.createConnection({ //con在get外可能會出錯
     host: "localhost",
     user: "root",
     password: "123456",
@@ -127,51 +126,112 @@ module.exports = function (app) {
 
   //輸入 本月的第幾天 會回傳七天日期的陣列
   function setMyDate(date) {
-    var dates = Array(7)
+    // var dates = Array(7) 移到外面變成屬性試試
 
     for (i = 0; i < 7; i++) {
-
-      dates[i] = getFormatDate(date + i)  // (2)
+      one_week[i] = getFormatDate(date + i)  // (2)
     }
-    return dates
+    return one_week
   }
 
   var today = new Date();
 
 
+  var data = [{
+    roomID: '二號會議室',
+    starttime: '9:00',
+    endTime: '12:00',
+    opendate: '/201912/26',
+    department: '精神科',
+    meetingID: '123',
+    meetingName: '嗚嗚嗚屋'
+  }, {
+    roomID: '一號會議室',
+    starttime: '7:00',
+    endTime: '8:00',
+    opendate: '2019/12/27',
+    department: '婦產科',
+    meetingID: '123456',
+    meetingName: '嗨嗨嗨嗨嗨嗨'
+  }];
+//巢狀迴圈在讀取物件時會出錯
+  function getOneWeekData(one_week) { //input一個禮拜的日期並return一個禮拜的會議資料
+    var sorted_Data = []
+    for (i = 0; i < one_week.length; i++) {
+      for (j = 0; j < data.length; j++) {
+        if (data[j].opendate == one_week[i]) { //第一個會讀不到
+          sorted_Data[i] = data[j]
+        } else {
+          sorted_Data[i] = {
+            roomID: '',
+            starttime: '',
+            endTime: '',
+            opendate: '',
+            department: '',
+            meetingID: '',
+            meetingName: ''
+          }
+        }
+      }
+    }
+    return sorted_Data
+  }
+  one_week = setMyDate(today.getDate())//預設日期
+  console.log("資料", getOneWeekData(one_week))
+
   app.get('/reservation', isLoggedIn, function (req, res) {
+
     var roomid = req.body.room_search
     var search_date = req.body.searchdate
     console.log(roomid)
     res.render('reservation.ejs', {
       user: req.user,
-      data: '哭哭哭哭哭哭阿',
       table_day: setWeek(today.getDay()),
-      table_date: setMyDate(today.getDate()),
-      roomID: '哭哭阿',
+      table_date: one_week,
       roomID: roomid,
-      date: search_date
+      date: search_date,
+
+      data: data
     });
   });
 
-
-
+  //查詢的動作
   app.post('/datainsert', urlencodedParser, function (req, res) {
+    //var one_weekData = new Array();
     var search_room = req.body.meetingroom
-    var search_date = req.body.searchdate   
+    var search_date = req.body.searchdate
     var new_date = new Date(search_date)
-    res.render('reservation.ejs',
-      {
-        user: req.user,
+    one_week = setMyDate(new_date.getDate()) //一個禮拜的日期陣列
 
-        data: '哭哭哭哭哭哭阿',
-        table_day: setWeek(new_date.getDay()),
-        table_date: setMyDate(new_date.getDate()),
-        roomID: '哭哭阿',
-        roomID: search_room,
-        date: search_date
-      })
-    
+    var data_all = {};
+    var SQL_MeetingName = "select roomID,starttime,endTime,DATE_FORMAT(opendate," + "'" + "%Y/%m/%d" + "'" + ") as opendate,department,meetingID,meetingName from reservation";
+    con.query(SQL_MeetingName, function (err, rows) {
+      data_all.reservation = rows;
+      console.log('搜尋結果哭阿', data_all.reservation);
+      if (err) {
+        res.redirect('errorre');
+      } else {
+        res.render('reservation.ejs', {
+          user: req.user,
+          table_day: setWeek(new_date.getDay()),
+          table_date: one_week,
+          data: data_all.reservation,
+          roomID: search_room,
+          date: search_date
+        });
+      }
+    })
+
+    /*測試中先不render reservation
+        res.render('reservation.ejs',
+          {
+            user: req.user,
+            table_day: setWeek(new_date.getDay()),
+            table_date: one_week,
+            roomID: search_room,
+            date: search_date
+          })
+    */
 
     //var data = {};
     //var sqlforsearch = 'select * from reservation where (roomid="'+ searchroom +'" OR starttime="'+ searchstart +'"OR endtime="'+ searchend +'"OR opendate="'+ searchdate +'"OR department="'+ searchdepartment +'"OR meetingname="'+ searchtopic +'")'
